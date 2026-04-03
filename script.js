3e0a051f90c4de630397b2d81f7a8452c736b79c
mosev1474 <zaffedontknowhimself@gmail.com>
2026-04-03 13:50:20 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const correctDay = 15;
+const correctMonth = 6;
+
+function createHearts() {
+    const container = document.getElementById("heartsContainer");
+
+    for (let i = 0; i < 20; i++) {
+        const heart = document.createElement("div");
+        heart.classList.add("heart");
+
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = (Math.random() * 3 + 3) + "s";
+
+        container.appendChild(heart);
+    }
+}
+
+function checkBirthday() {
+    const day = parseInt(document.getElementById("day").value);
+    const month = parseInt(document.getElementById("month").value);
+
+    const error = document.getElementById("errorMessage");
+
+    if (day === correctDay && month === correctMonth) {
+        window.location.href = "NEXT_PAGE_LINK";
+    } else {
+        error.innerText = "غلط... بس جربي تاني 🥺";
+    }
+}
+
+window.onload = () => {
+    createHearts();
+
+    setTimeout(() => {
+        document.getElementById("loadingSpinner").style.display = "none";
+    }, 1000);
+};
